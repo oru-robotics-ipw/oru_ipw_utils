19616c3919e7ada17d3f1f2f6a89508f2f2520c2
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2019-12-27 17:13:37 +0100

    Compute some statistics separately for manual/auto

--- src/oru_ipw_system_analysis/odom.py
+++ src/oru_ipw_system_analysis/odom.py
@@ -5,6 +5,9 @@ import rospy
 
 from .analysis import Analysis
 
+_MANUAL_CONTROL = 0
+_AUTO_CONTROL = 1
+
 
 class OdomAnalysis(Analysis):
     """Odometry analysis"""
@@ -14,10 +17,13 @@ class OdomAnalysis(Analysis):
         old_ts = None
         ts_start = None
         distance_total = 0.0
+        control_mode = _MANUAL_CONTROL
+        distance_categories = {_MANUAL_CONTROL: 0.0, _AUTO_CONTROL: 0.0}
         max_speed = 0.0
         time_active = rospy.Duration(0)
+        time_categories = {_MANUAL_CONTROL: rospy.Duration(0), _AUTO_CONTROL: rospy.Duration(0)}
         for bag in self._bags:
-            for topic, msg, t in bag.read_messages(topics=('/hrp/odom',)):
+            for topic, msg, t in bag.read_messages(topics=('/hrp/odom', '/move_base/goal', '/move_base/result')):
                 ts = msg.header.stamp  # type: rospy.Time
                 if not ts_start:
                     ts_start = ts
@@ -30,21 +36,31 @@ class OdomAnalysis(Analysis):
                         delta_distance = np.linalg.norm(delta_vec)  # type: float
                         # Compute running distance
                         distance_total += delta_distance
+                        distance_categories[control_mode] += delta_distance
                         # Compute active time
                         if delta_distance > 0.0001:
                             delta_t = (ts - old_ts)
                             time_active += delta_t
+                            time_categories[control_mode] += delta_t
                             speed = delta_distance / delta_t.to_sec()
                             if speed > max_speed:
                                 max_speed = speed
                     old_position = vec
                     old_ts = ts
+                elif topic == '/move_base/result':
+                    control_mode = _MANUAL_CONTROL
+                elif topic == '/move_base/goal':
+                    control_mode = _AUTO_CONTROL
 
         total_time = (ts - ts_start).to_sec()
         active_time = time_active.to_sec()
         print("Driven distance: %r m" % distance_total)
+        print("         Manual: %r m" % distance_categories[_MANUAL_CONTROL])
+        print("           Auto: %r m" % distance_categories[_AUTO_CONTROL])
         print("Max speed: %r m/s" % max_speed)
         print("Average speed (active time): %r m/s" % (distance_total / active_time))
         print("Average speed (total time): %r m/s" % (distance_total / total_time))
         print("Active duration: %r s" % active_time)
+        print("         Manual: %r s" % time_categories[_MANUAL_CONTROL].to_sec())
+        print("           Auto: %r s" % time_categories[_AUTO_CONTROL].to_sec())
         print("Total duration: %r s" % total_time)
